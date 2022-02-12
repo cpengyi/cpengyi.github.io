63fb9ee64029ece19f6a0414eeb3792e36a36e11
Pengyi Chen <93240694+cpengyi@users.noreply.github.com>
2022-02-12 17:24:42 -0500

    Add files via upload

---
+++ 2022 V Day/for you.py
@@ -0,0 +1,16 @@
+"""Love you infinite"""
+__author__ = "你的宝贝老婆"
+
+i: int = 1251
+name: str = input("你是我的宝贝老公lkp嘛？(输入是/不是) ")
+if name == "是":
+    while i < 1314:
+        print("爱宝贝kk老公每辈子！！！")
+else:
+    name = input("诶，真的不是嘛？（不是哦.../骗你的啦小傻瓜~）")
+    if name == "骗你的啦小傻瓜~":
+        while i < 1314:
+            print("爱坏坏kk老公每辈子！！！")
+    else:
+        print("那好吧...那我去找找我老公在哪里...")
+        quit()
